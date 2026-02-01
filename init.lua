4644c7ec7d05eaaa7a44b28d7f2c4c9c37bd8eb3
yandvo <yandvo@gmail.com>
2026-02-01 13:15:03 -0500

    added notify for prettier notifications

--- init.lua
+++ init.lua
@@ -17,6 +17,7 @@ vim.opt.number = false            -- line numbers
 vim.opt.signcolumn = "yes"        -- always show sign column
 vim.opt.wrap = false              -- no line wrap
 vim.opt.scrolloff = 8             -- margin above/below cursor
+vim.opt.scroll = 12               -- set scroll amount
 vim.opt.sidescrolloff = 8         -- margin left/right of cursor
 vim.opt.fillchars = { eob = " " } -- hide ~ on end of buffer
 vim.opt.exrc = true               -- allows local cfgs
@@ -25,3 +26,6 @@ vim.opt.exrc = true               -- allows local cfgs
 require("config.lazy")
 require("config.keymaps")
 require("config.user_functions")
+
+-- enable nvim notify
+vim.notify = require("notify")
