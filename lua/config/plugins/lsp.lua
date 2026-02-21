8b1939dfb0a29a787bd3103e98f854f05accecf9
yandvo <yandvo@gmail.com>
2026-02-21 12:08:36 -0500

    move from pyright lsp to ty

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -16,24 +16,6 @@ return {
     },
     config = function()
       -- Server configs
-      vim.lsp.config('basedpyright', {
-        settings = {
-          basedpyright = {
-            analysis = {
-              -- make it less strict:
-              typeCheckingMode = "basic",       -- or "standard", or whatever level you prefer
-              diagnosticMode = "openFilesOnly", -- so only open files are checked
-              -- optionally override specific diagnostics
-              diagnosticSeverityOverrides = {
-                -- e.g. turn some errors into warnings or suppress them
-                reportMissingTypeStubs = "warning",
-                reportPrivateImportUsage = "none",
-              },
-            },
-          },
-        },
-      })
-
       vim.lsp.config('tinymist', {
         settings = {
           formatterMode = "typstyle",
@@ -43,7 +25,7 @@ return {
       -- setup clang for use with esp-idf
       vim.lsp.config("clangd", {
         cmd = {
-          "/home/ydvo/.espressif/tools/esp-clang/esp-20.1.1_20250829/esp-clang/bin/clangd",
+          "/home/ydvo/.espressif/tools/esp-clang/esp-19.1.2_20250312/esp-clang/bin/clangd",
           "--background-index",
           "--compile-commands-dir=build.clang",
           "--query-driver=/home/ydvo/.espressif/tools/**",
@@ -63,14 +45,13 @@ return {
       })
 
       -- enable server configurations
-      vim.lsp.enable("lua_ls")
-      vim.lsp.enable("rust_analyzer")
-      vim.lsp.enable("tinymist")
-      vim.lsp.enable("basedpyright")
+      vim.lsp.enable('lua_ls')
+      vim.lsp.enable('rust_analyzer')
+      vim.lsp.enable('tinymist')
+      vim.lsp.enable('ty')
       vim.lsp.enable('ruff')
       vim.lsp.enable('clangd')
       vim.lsp.enable('sourcekit')
-      vim.lsp.enable('asm_lsp')
     end,
   }
 }
